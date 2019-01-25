6be2fd9e307cf90deeeabb09584bae853ad35da4
Hersh Shafer <hershs@gmail.com>
2019-01-25 15:57:57 +0200

    add comment

--- src/main/clojure/tst_clojure.clj
+++ src/main/clojure/tst_clojure.clj
@@ -3,8 +3,10 @@
 (def event {:aa 1 :bb 2 :cc 3})
 (def dims [:aa :cc])
 
+; short crazy version
 (prn ((apply juxt dims) event))
 
+; more readable version, if you can call clojure 'readable'
 (prn (->> event
           (filter #(some #{(first %)} dims))
           (map #(second %))
